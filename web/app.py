4a1c578d96dcdd717a6ddbbb973e272c9cc7082c
zipoo69 <petr.vosicky@post.cz>
2026-04-27 08:58:48 +0200

    uprava konce

--- web/app.py
+++ web/app.py
@@ -19,7 +19,7 @@ def index():
 def convert():
     file = request.files["file"]
 
-    name = os.path.splitext(secure_filename(file.filename))[0]  # ← tato změna
+    name = os.path.splitext(secure_filename(file.filename))[0]
 
     pdf_path = os.path.join(UPLOAD_FOLDER, name + ".pdf")
     docx_path = os.path.join(UPLOAD_FOLDER, name + ".docx")
@@ -37,7 +37,7 @@ def convert():
     print(result.stderr)
 
     if not os.path.exists(docx_path):
-        return f"Konverze selhala:\n{result.stderr}"
+        return f"Konverze selhala:\n{result.stderr}", 500
 
     return send_file(docx_path, as_attachment=True)
 
